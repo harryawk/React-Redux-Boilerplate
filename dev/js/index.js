da21f1e15114da5ecc7acd7dc9694868ac95f4b0
harryawk <harry.kambey@gmail.com>
2017-07-17 23:43:57 +0700

    reactjs - redux tutorial until mapStateToProps

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,8 +1,16 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import allReducers from './reducers/index.js';
+import App from './components/app'
+
+const store = createStore(allReducers);
 
 ReactDOM.render(
-    <h1>Hello World</h1>,
+	<Provider store={store}>
+		<App />
+	</Provider>,
     document.getElementById('root')
 );
